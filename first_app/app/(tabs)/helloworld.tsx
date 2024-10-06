91d92b56933c179e0794eabf4d54393c64b991cc
Yukenshi7230 <61129075+Angellington@users.noreply.github.com>
2024-10-06 18:15:09 -0300

    Starting Documentation Studies

---
+++ first_app/app/(tabs)/helloworld.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import  { Text, View } from 'react-native';
+
+const YourApp = () => {
+    return (
+        <View
+        style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+            <Text>Hello World!! - Olá Mundo! - こんにちは世界 - Konichuwa Sekai!</Text>
+        </View>
+    );
+};
+
+export default YourApp;
